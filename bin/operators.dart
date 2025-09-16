b3ad462ccae6625dfc22649123589431ea0c7dc0
sudipeb <sudip.koirala@ebpearls.com>
2025-09-16 14:17:06 +0545

    simple operators exercise finished

---
+++ bin/operators.dart
@@ -0,0 +1,14 @@
+void main(){
+  const firstNumber =5.5;
+  const secondNumber =10.5;
+  const thirdNumber = firstNumber + secondNumber ; //ADDITION
+  const thirdNumber1 = firstNumber - secondNumber ; // SUBTRACTION
+  const thirdNumber2 = firstNumber * secondNumber ; // MULTIPLICAITON
+  const thirdNumber3 = firstNumber/secondNumber ; // DIVISION
+  const int thirdNumber4 = firstNumber~/secondNumber; // only returns the integer value and can be used in case of returing the integer values only form the double values.
+  print(thirdNumber);
+  print(thirdNumber1);
+  print(thirdNumber2);
+  print(thirdNumber3);
+  print(thirdNumber4);
+}
